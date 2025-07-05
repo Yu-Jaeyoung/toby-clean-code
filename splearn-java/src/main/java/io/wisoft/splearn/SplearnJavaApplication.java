720928f5d1c9e802a4dff7f57f39e99fb8dd6c28
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-07-05 20:20:00 +0900

    spring initial commit, docker-compose set

---
+++ splearn-java/src/main/java/io/wisoft/splearn/SplearnJavaApplication.java
@@ -0,0 +1,13 @@
+package io.wisoft.splearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplearnJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplearnJavaApplication.class, args);
+    }
+
+}
